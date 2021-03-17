b5e386da73b0e5d273116f573df24ad208ed3ae2
Sean Randell <s3658051@student.rmit.edu.au>
2021-03-17 18:07:33 +1100

    Initial Scripts for player health and Interactables

---
+++ IDMP_A_Team_Raven_Project/Assets/Scripts/Objects/Terminal.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Terminal : Interactable
+{
+    public GameObject dialogueBox;
+    public Text dialogueText;
+    public string dialogue;
+
+    // Update is called once per frame
+    public virtual void Update()
+    {
+        if (Input.GetButtonDown("interact") && playerInRange)
+        {
+            if (dialogueBox.activeInHierarchy)
+            {
+                dialogueBox.SetActive(false);
+            }
+            else
+            {
+                dialogueBox.SetActive(true);
+                dialogueText.text = dialogue;
+            }
+        }
+    }
+    public override void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            dialogueBox.SetActive(false);
+            base.OnTriggerExit2D(other);
+        }
+    }
+}
---
+++ IDMP_A_Team_Raven_Project/Assets/Scripts/Player/PlayerHealthManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthManager : MonoBehaviour
+{
+    public FloatValue currentHealth;
+    public SignalSender playerHealthSignal;
+
+    //TODO - call in whatever script handles player being hit in combat
+
+}
