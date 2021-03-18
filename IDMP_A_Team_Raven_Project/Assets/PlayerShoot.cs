336e2744310d3e40946dcb4e6d8bcf71d499a208
Louis Baudinette (s3668025) <s3668025@student.rmit.edu.au>
2021-03-18 14:23:08 +1100

    Added gun aiming
    Also fixed collisions, I think (?)

---
+++ IDMP_A_Team_Raven_Project/Assets/PlayerShoot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject gun;
+    public LineRenderer lr;
+
+    Vector2 mousePos;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 lookDir = (Vector3) mousePos - gun.transform.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        gun.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+        DrawLine();
+    }
+
+    private void DrawLine()
+    {
+        lr.SetPosition(0, gun.transform.position);
+        lr.SetPosition(1, mousePos);
+
+    }
+}
