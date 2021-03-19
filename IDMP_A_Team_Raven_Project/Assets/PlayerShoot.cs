56c209b09f65cba37911c6c3f6c8abf24f214db3
Louis Baudinette (s3668025) <s3668025@student.rmit.edu.au>
2021-03-19 16:51:33 +1100

    Implemented basic aiming toggle and crossbow sprite

--- IDMP_A_Team_Raven_Project/Assets/PlayerShoot.cs
+++ IDMP_A_Team_Raven_Project/Assets/PlayerShoot.cs
@@ -5,28 +5,56 @@ using UnityEngine;
 public class PlayerShoot : MonoBehaviour
 {
     public Camera cam;
-    public GameObject gun;
+    public GameObject weapon;
+    public SpriteRenderer weaponSprite;
     public LineRenderer lr;
 
     Vector2 mousePos;
     void Start()
     {
-
+        weaponSprite.enabled = false;
+        lr.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 lookDir = (Vector3) mousePos - gun.transform.position;
-        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
-        gun.transform.rotation = Quaternion.Euler(0f, 0f, angle);
-        DrawLine();
+        // disable line renderer and weapon sprite when RMB is released
+        if (Input.GetMouseButtonUp(1))
+        {
+            weaponSprite.enabled = false;
+            lr.enabled = false;
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            // get mouse position and angle relative to player
+            mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 lookDir = (Vector3)mousePos - weapon.transform.position;
+            float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+
+            // rotate weapon to face mouse direction
+            weapon.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+            weaponSprite.enabled = true;
+            lr.enabled = true;
+            // flip sprite if crossbow on other side of player
+            if (angle <= -90f || angle > 90)
+            {
+                weaponSprite.flipY = true;
+            }
+            else
+            {
+                weaponSprite.flipY = false;
+            }
+
+            DrawLine();
+        }
     }
 
     private void DrawLine()
     {
-        lr.SetPosition(0, gun.transform.position);
+        lr.SetPosition(0, weapon.transform.position);
         lr.SetPosition(1, mousePos);
 
     }
