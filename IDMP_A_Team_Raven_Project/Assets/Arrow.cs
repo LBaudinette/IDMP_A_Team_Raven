73beff032cf96777abfd6c536f494fe33fe4bc84
Louis Baudinette (s3668025) <s3668025@student.rmit.edu.au>
2021-03-21 14:57:31 +1100

    set up arrow/player prefabs

---
+++ IDMP_A_Team_Raven_Project/Assets/Arrow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+
+    public Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // on enemy collision, damage them and potentially apply knockback based on current rb2d force
+    }
+}
