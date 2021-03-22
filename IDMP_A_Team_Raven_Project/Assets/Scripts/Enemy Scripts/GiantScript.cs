e76cd75ff2a5fd461d2295ecfb2a596f590a6aff
rmit-s3722631-Daniel-Snowsill <41409058+rmit-s3722631-Daniel-Snowsill@users.noreply.github.com>
2021-03-22 19:04:10 +1100

    Added Golem Animations

--- IDMP_A_Team_Raven_Project/Assets/Scripts/Enemy Scripts/GiantScript.cs
+++ IDMP_A_Team_Raven_Project/Assets/Scripts/Enemy Scripts/GiantScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class GiantScript : MonoBehaviour
 {
@@ -13,15 +14,41 @@ public class GiantScript : MonoBehaviour
     private bool canAttack = true;
     private Coroutine coroutine;
 
+    private Animator animator;
+    public AIPath aiPath;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindWithTag("Player");
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 absVelocityX = aiPath.desiredVelocity.normalized;
+        //Check if the enemy is moving right
+        if (aiPath.desiredVelocity.x > 0f) {
+            Debug.Log("velocity: " + aiPath.desiredVelocity);
+            animator.SetFloat("moveX", absVelocityX.x);
+
+            animator.SetBool("isMoving", true);
+        }
+        //Check if the enemy is moving left
+        else if(aiPath.desiredVelocity.x < 0f) {
+            Debug.Log("velocity: " + aiPath.desiredVelocity.x);
+            animator.SetFloat("moveX", absVelocityX.x);
+
+            animator.SetBool("isMoving", true);
+        }
+        //Enemy is not moving
+        else {
+            animator.SetBool("isMoving", false);
+            animator.SetFloat("moveX", 0);
+        }
+
+
         //check for the distance between the enemy and the player
         float distance = Vector2.Distance(gameObject.transform.position, 
             player.transform.position);
@@ -34,14 +61,31 @@ public class GiantScript : MonoBehaviour
         //create a special area if enough time has passed between attacks
         if (canAttack) {
             Debug.Log("Attack");
-            Instantiate(areaAttack, transform.position, transform.rotation);
+
+            //Set the isAttacking boolean in the animator
+            animator.SetBool("isAttacking", true);
+            animator.SetBool("isMoving", false);
+
             canAttack = false;
-            coroutine = StartCoroutine(startAttackCooldown());
+
         }
     }
-    
+
+    private void endAttack() {
+        Debug.Log("Finished Attack");
+        //Create the area attack
+        Instantiate(areaAttack, transform.position, transform.rotation);
+
+        animator.SetBool("isAttacking", false);
+        animator.SetBool("isMoving", false);
+
+        coroutine = StartCoroutine(startAttackCooldown());
+    }
+
+
     private IEnumerator startAttackCooldown() {
-        while(attackTimer < attackDelay) {
+
+        while (attackTimer < attackDelay) {
             attackTimer += Time.deltaTime;
 
             yield return null;
@@ -50,6 +94,8 @@ public class GiantScript : MonoBehaviour
         canAttack = true;
         attackTimer = 0f;
     }
+
+    
 
     public void takeDamage(float damage, float force, Vector2 angle) {
 
