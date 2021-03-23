160edc6af3536b381b1b8b686a8649d6f4f95eb3
rmit-s3722631-Daniel-Snowsill <41409058+rmit-s3722631-Daniel-Snowsill@users.noreply.github.com>
2021-03-23 20:11:07 +1100

    Changes to animation controllers

---
+++ IDMP_A_Team_Raven_Project/Assets/Scripts/Enemy Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class Enemy : MonoBehaviour
+{
+    protected Animator animator;
+    protected bool canAttack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //
+    }
+
+    protected void startMoving(Transform target) {
+
+    }
+
+    protected virtual void startAttack() {
+
+    }
+}
---
+++ IDMP_A_Team_Raven_Project/Assets/Scripts/Enemy Scripts/SwarmEnemyScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwarmEnemyScript : Enemy
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void startAttack() {
+
+        if (canAttack) {
+            Debug.Log("Attack");
+
+            //Set the isAttacking boolean in the animator
+            base.animator.SetBool("isAttacking", true);
+            animator.SetBool("isMoving", false);
+
+            canAttack = false;
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.tag == "Player") {
+            startAttack();
+        }
+    }
+}
