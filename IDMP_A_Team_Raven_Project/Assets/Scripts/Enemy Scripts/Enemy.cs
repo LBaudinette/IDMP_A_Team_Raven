6c90783f9e388e0b956c71d9533388d8904ba0b6
rmit-s3722631-Daniel-Snowsill <41409058+rmit-s3722631-Daniel-Snowsill@users.noreply.github.com>
2021-03-24 17:37:46 +1100

    Started pathfinding on Enemy superclass

--- IDMP_A_Team_Raven_Project/Assets/Scripts/Enemy Scripts/Enemy.cs
+++ IDMP_A_Team_Raven_Project/Assets/Scripts/Enemy Scripts/Enemy.cs
@@ -7,17 +7,45 @@ public class Enemy : MonoBehaviour
 {
     protected Animator animator;
     protected bool canAttack;
+    protected Rigidbody2D rb;
+
+    public Transform leftPlayer, rightPlayer;
+
+    Path path;
+    Seeker seeker;
+    int currentWaypoint = 0;
+    float speed = 200f;
+    bool isEndOfPath = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        //TODO: assign leftPlayer and rightPlayer from the children of player object
+
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        seeker.StartPath(rb.position, leftPlayer.position, OnPathComplete);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //
+        if (path == null)
+            return;
+
+        if(currentWaypoint >= path.vectorPath.Count) {
+            isEndOfPath = true;
+            return;
+        }
+    }
+
+    protected void OnPathComplete(Path p) {
+
+        //if there is no error in the calculated path
+        if (!p.error) {
+            path = p;
+
+        }
     }
 
     protected void startMoving(Transform target) {
