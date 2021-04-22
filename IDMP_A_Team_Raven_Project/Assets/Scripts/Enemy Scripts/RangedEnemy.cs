a6166555b40443d8b8d37547b24b4b15e786497a
rmit-s3722631-Daniel-Snowsill <41409058+rmit-s3722631-Daniel-Snowsill@users.noreply.github.com>
2021-04-22 16:58:41 +1000

    Implemented teleporting

--- IDMP_A_Team_Raven_Project/Assets/Scripts/Enemy Scripts/RangedEnemy.cs
+++ IDMP_A_Team_Raven_Project/Assets/Scripts/Enemy Scripts/RangedEnemy.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RangedEnemy : MonoBehaviour
-{
+public class RangedEnemy : MonoBehaviour {
 
     private Vector2 playerPos;
     private float firingDelay = 2f;          //The delay between firing projectiles
@@ -14,26 +13,35 @@ public class RangedEnemy : MonoBehaviour
     public Transform rotationPoint;
     public float teleTriggerDistance = 1;          //How close the player must be to start teleporting
 
-    private float teleportTimer = 0;     
+    private float teleportTimer = 0;
     public float teleportDelay = 3;     //How long it takes to teleport
     public float teleportDistance = 3f;
     private bool isTeleporting = false;
 
-    public Transform leftRaycastPoint, topRaycastPoint, 
+    public Transform leftRaycastPoint, topRaycastPoint,
         rightRaycastPoint, botRaycastPoint;
     public LayerMask layerDetection;
 
     private Coroutine coroutine;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    private struct Raycast{
+
+        public Ray2D ray;
+        public RaycastHit2D raycastHit;
+
+        public Raycast( Ray2D Ray, RaycastHit2D RaycastHit2D) {
+            ray = Ray;
+            raycastHit = RaycastHit2D;
+        }
+    }
+
+// Start is called before the first frame update
+void Start() {
         playerPos = GameObject.FindWithTag("Player").transform.position;
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         //Update the player position every frame
         playerPos = GameObject.FindWithTag("Player").transform.position;
 
@@ -56,7 +64,7 @@ public class RangedEnemy : MonoBehaviour
 
         //Find the angle between the two objects using the vector we just calculated in degrees
         float angleFloat = Mathf.Atan2(vectorDifference.y, vectorDifference.x) * Mathf.Rad2Deg;
-        
+
         // update rotation of fire point
         rotationPoint.rotation = Quaternion.Euler(0f, 0f, angleFloat);
 
@@ -72,34 +80,101 @@ public class RangedEnemy : MonoBehaviour
         Debug.Log("Starting Teleport");
 
         //Start the teleport timer
-        while(teleportTimer < teleportDelay) {
+        while (teleportTimer < teleportDelay) {
             Debug.Log("Timer: " + teleportTimer);
             teleportTimer += Time.deltaTime;
 
             yield return null;
         }
 
-        
+
 
         teleport();
     }
 
     void teleport() {
+        
+        var hitRaycasts = new List<Raycast>();
+        var emptyRaycasts = new List<Raycast>();
+        var rays = new List<Raycast>();
+
         //Teleport
         //Fire 4 raycasts to check for available teleports
+        
         RaycastHit2D leftHit;
         RaycastHit2D topHit;
         RaycastHit2D rightHit;
         RaycastHit2D botHit;
 
-        leftHit = Physics2D.Raycast(leftRaycastPoint.position, Vector2.left, teleTriggerDistance, layerDetection);
-        topHit = Physics2D.Raycast(topRaycastPoint.position, Vector2.up, teleTriggerDistance, layerDetection);
-        rightHit = Physics2D.Raycast(rightRaycastPoint.position, Vector2.right, teleTriggerDistance, layerDetection);
-        botHit = Physics2D.Raycast(botRaycastPoint.position, Vector2.down, teleTriggerDistance, layerDetection);
+        Ray2D leftRay = new Ray2D(leftRaycastPoint.position, Vector2.left);
+        Ray2D topRay = new Ray2D(topRaycastPoint.position, Vector2.up);
+        Ray2D rightRay = new Ray2D(rightRaycastPoint.position, Vector2.right);
+        Ray2D botRay = new Ray2D(botRaycastPoint.position, Vector2.down);
 
-        //Teleport the furthest distance
+     
 
-        transform.Translate(Vector2.left * teleTriggerDistance);
+        leftHit = Physics2D.Raycast(leftRay.origin, leftRay.direction, teleTriggerDistance, layerDetection);
+        topHit = Physics2D.Raycast(topRay.origin, topRay.direction, teleportDistance, layerDetection);
+        rightHit = Physics2D.Raycast(rightRay.origin, rightRay.direction, teleportDistance, layerDetection);
+        botHit = Physics2D.Raycast(botRay.origin, botRay.direction, teleportDistance, layerDetection);
+
+        Raycast leftRaycast = new Raycast(leftRay, leftHit);
+        Raycast topRaycast = new Raycast(topRay, topHit);
+        Raycast rightRaycast = new Raycast(rightRay, rightHit);
+        Raycast botRaycast = new Raycast(botRay, botHit);
+
+        rays.Add(leftRaycast);
+        rays.Add(topRaycast);
+        rays.Add(rightRaycast);
+        rays.Add(botRaycast);
+
+        //Assign longest hit to an existing hit so we can compare to others
+        RaycastHit2D longestHit = leftHit;
+
+        //Check if each one hit something
+        foreach(Raycast raycast in rays) {
+            if (raycast.raycastHit.collider != null)
+                hitRaycasts.Add(raycast);
+            else
+                emptyRaycasts.Add(raycast);
+
+        }
+
+        Raycast longestRaycast;
+        //if no walls were hit, then teleport into an open space
+        if (hitRaycasts.Count == 0) {
+            //assign the first element as the longest
+            longestRaycast = emptyRaycasts[0];
+
+            foreach(Raycast raycast in emptyRaycasts) {
+                if (raycast.raycastHit.distance > longestRaycast.raycastHit.distance)
+                    longestRaycast = raycast;
+            }
+
+            //pick a random direction
+            int randomIndex = Random.Range(0, emptyRaycasts.Count - 1);
+
+            //Teleport to the open space
+            //transform.Translate(longestRaycast.ray.direction * teleportDistance);
+            transform.Translate(emptyRaycasts[randomIndex].ray.direction * teleportDistance);
+        }
+        //if there are no empty spaces, teleport next to a wall
+        else {
+            longestRaycast = hitRaycasts[0];
+
+            foreach (Raycast raycast in hitRaycasts) {
+                if (raycast.raycastHit.distance > longestRaycast.raycastHit.distance)
+                    longestRaycast = raycast;
+            }
+
+            //Teleport to the walls position
+            transform.Translate(longestRaycast.ray.direction * teleportDistance);
+
+        }
+
+        
+
+        
 
         isTeleporting = false;
         //Reset teleport timer
