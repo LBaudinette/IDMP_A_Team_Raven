cdbb2ddfaf44343968517d78c11988a38a1d2916
Daniel <s3722631@student.rmit.edu.au>
2021-05-02 21:39:10 +1000

    Changed hit stop duration and added enemy attack hitboxes

---
+++ IDMP_A_Team_Raven_Project/Assets/Scripts/Enemy Scripts/DealHitMeleeEnemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DealHitMeleeEnemy : MonoBehaviour
+{
+
+    public float damage;
+    public float knockback;
+    public GameObject parentPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public float getDamage() {
+        return damage;
+    }
+
+    public float getKnockback() {
+        return knockback;
+    }
+    public GameObject getParentPos() {
+        return parentPosition;
+    }
+
+
+
+    //Deal hit melee
+    //Make enemy hitbox tag
+}
