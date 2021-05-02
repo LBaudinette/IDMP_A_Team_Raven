a2cb8d60688f6bcab13bab02755c1ff873671dbc
s3658051 <s3658051@student.rmit.edu.au>
2021-05-02 23:25:49 +1000

    Update boolvalue bug

--- IDMP_A_Team_Raven_Project/Assets/Scripts/ScriptableObjects/BoolValue.cs
+++ IDMP_A_Team_Raven_Project/Assets/Scripts/ScriptableObjects/BoolValue.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
-[System.Serializable]
-public class BoolValue : ScriptableObject
+//[System.Serializable]
+public class BoolValue : ScriptableObject, ISerializationCallbackReceiver
 {
     public bool initialValue;
     public bool runTimeValue;
+
+    public void OnAfterDeserialize()
+    {
+        runTimeValue = initialValue;
+    }
+
+    public void OnBeforeSerialize()
+    {
+    }
 }
 
