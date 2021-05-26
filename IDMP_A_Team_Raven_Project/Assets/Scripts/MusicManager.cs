f6faa537a7dd1fcc66e17140b4cfb66156d51ba8
Louis Baudinette (s3668025) <s3668025@student.rmit.edu.au>
2021-05-26 15:28:11 +1000

    added music, removed light from fire giant atk

---
+++ IDMP_A_Team_Raven_Project/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicManager : MonoBehaviour
+{
+
+    public AudioClip ambient;
+    public AudioClip battle;
+
+    private AudioSource musicSource;
+    public float volume;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicSource = GetComponent<AudioSource>();
+        musicSource.volume = volume;
+        musicSource.loop = true;
+        musicSource.clip = ambient;
+        musicSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
